e6d646660e7ef1207f89b9f030086a3a5010b822
danimarcu99 <35343896+danimarcu99@users.noreply.github.com>
2020-04-10 14:56:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from GA import GA
+#ga=GA('D:\AI\Lab4\easy1.txt')
+#ga=GA('D:\AI\Lab4\easy2.txt')
+#ga=GA('D:\AI\Lab4\easy3.txt')
+#ga=GA('D:\AI\Lab4\hardE.txt')
+ga=GA('D:\AI\Lab4\mediumF.txt')
+solution=ga.solution()
+print(solution)
