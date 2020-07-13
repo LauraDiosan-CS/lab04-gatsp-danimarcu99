bdb50264a907553f52f90113fabd664993da53a9
Dani Marcu <35343896+dani-marcu@users.noreply.github.com>
2020-07-13 19:05:19 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from GA import GA
-#ga=GA('D:\AI\Lab4\easy1.txt')
-#ga=GA('D:\AI\Lab4\easy2.txt')
-#ga=GA('D:\AI\Lab4\easy3.txt')
-#ga=GA('D:\AI\Lab4\hardE.txt')
-ga=GA('D:\AI\Lab4\mediumF.txt')
-solution=ga.solution()
-print(solution)
